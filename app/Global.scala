d89141c48c88c3506a37edc6674b6b5db4b3ab2f
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-06-19 17:35:58 -0400

    Made it so all that non logged in users connot access other pages without first logging in and added custom 404 page

---
+++ app/Global.scala
@@ -0,0 +1,28 @@
+import scala.concurrent._
+import play.api._
+import play.api.mvc._
+import play.api.mvc.RequestHeader
+import play.api.{GlobalSettings, Logger}
+
+object Global extends GlobalSettings {
+  override def onRouteRequest(request: RequestHeader) = {
+    println("recieved request for [%s]" format request)
+    if(!request.uri.equals("/login") && !request.session.get("loginTime").isDefined){
+    	var redirectedRequest = request.copy(request.id, request.tags, "/login", "/login", "GET", request.version, request.queryString, request.headers, request.remoteAddress)
+    	println("redirected request to [%s]" format redirectedRequest)
+    	super.onRouteRequest(redirectedRequest)
+    }
+    else{
+    	super.onRouteRequest(request)
+    }
+  }
+
+  override def onRequestCompletion(request: RequestHeader) {
+	  println("completed request for [%s]" format request)
+	  super.onRequestCompletion(request)
+  }
+  
+  override def onHandlerNotFound(request: RequestHeader) =  {
+	  Future.successful(Results.Ok(views.html.invalidPage()))
+  }
+}
