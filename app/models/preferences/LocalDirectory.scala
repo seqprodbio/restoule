dfa9687638eefb54fc14b87f5448bd6a0cf01620
Tony DeBat <tony.debat@oicr.on.ca>
2014-06-19 14:44:49 -0400

    Local directory model to locate files on the local filesystem.

---
+++ app/models/preferences/LocalDirectory.scala
@@ -0,0 +1,18 @@
+package models.preferences
+
+case class LocalDirectory(id: Long, directory: String, totalfiles: Integer)
+
+object LocalDirectory {
+  var localDirectories = Set(
+    LocalDirectory(1L, "/usr/local/ega", 33),
+    LocalDirectory(2L, "/test/dir", 567),
+    LocalDirectory(3L, "/home/user", 92))
+
+  def findAll = localDirectories.toList.sortBy(_.directory)
+
+  def findById(id: Long) = localDirectories.find(_.id == id)
+
+  def add(localDirectory: LocalDirectory) {
+    localDirectories = localDirectories + localDirectory
+  }
+}
