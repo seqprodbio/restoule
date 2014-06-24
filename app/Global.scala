f20ee66eee465b23d55711715fdb204569973a4a
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-06-24 14:48:34 -0400

    Updated Main Page and added page to set name of new EGA Release or select which previous release to view

--- app/Global.scala
+++ app/Global.scala
@@ -8,7 +8,7 @@ import play.api.{ GlobalSettings, Logger }
 object Global extends GlobalSettings {
    override def onRouteRequest(request: RequestHeader) = {
       println("recieved request for [%s]" format request)
-      if (!request.uri.equals("/login") && !request.session.get("loginTime").isDefined) {
+      if (request.method.equals("GET") && !request.uri.equals("/login") && !request.session.get("loginTime").isDefined) {
          //TODO: Make it so that after logging in successfully, the user gets redirected to the page that they originally wanted to go to
          //Note: changing the values in request.headers will not change session information
          var redirectedRequest = request.copy(request.id, request.tags, "/login", "/login", "GET", request.version, request.queryString, request.headers, request.remoteAddress)
---
+++ app/controllers/egaReleases.scala
@@ -0,0 +1,80 @@
+package controllers;
+
+import play.api._
+import play.api.mvc._
+import play.api.data.{ Form }
+import play.api.data.Forms._
+
+object EgaReleases extends Controller {
+
+   val addReleaseMapping = single("releaseName" -> text(minLength = 1))
+
+   val addReleaseForm = Form(addReleaseMapping)
+
+   val selectReleaseForm = Form("select" -> list(nonEmptyText))
+
+   def viewEgaReleases = Action { implicit request =>
+      Ok(views.html.egaReleasesPage(getReleaseNames()))
+   }
+
+   def addEgaRelease = Action { implicit request =>
+      var newSession = request.session
+      if (request.session.get("invalidViewRelease").isDefined) {
+         newSession = newSession - "invalidViewRelease"
+      }
+      addReleaseForm.bindFromRequest().fold(
+         formWithErrors => {
+            newSession = newSession + ("invalidNewReleaseName" -> "The release name must not be empty!")
+            Redirect(routes.EgaReleases.viewEgaReleases()).withSession(newSession)
+         },
+         releaseName => {
+            if (releaseNameAlreadyExists(releaseName)) {
+               newSession = newSession + ("invalidNewReleaseName" -> "There already exists a release with that name!")
+               Redirect(routes.EgaReleases.viewEgaReleases()).withSession(newSession)
+            } else {
+               newSession = newSession + ("newReleaseName" -> releaseName)
+               if (request.session.get("invalidNewReleaseName").isDefined) {
+                  newSession = newSession - "invalidNewReleaseName"
+               }
+               Redirect(routes.EgaReleases.viewEgaReleases()).withSession(newSession)
+            }
+         })
+   }
+
+   def openEgaRelease = Action { implicit request =>
+      selectReleaseForm.bindFromRequest.fold(
+         formWithErrors => {
+            Redirect(routes.EgaReleases.viewEgaReleases())
+         },
+         values => {
+            var newSession = request.session
+            if (request.session.get("invalidNewReleaseName").isDefined) {
+               newSession = newSession - "invalidNewReleaseName"
+            }
+            if (values.size > 0) {
+               println(values(0))
+               if (request.session.get("invalidViewRelease").isDefined) {
+                  newSession = newSession - "invalidViewRelease"
+               }
+               newSession = newSession + ("viewReleaseName" -> values(0))
+
+               Redirect(routes.EgaReleases.viewEgaReleases()).withSession(newSession)
+            } else {
+               newSession = newSession + ("invalidViewRelease" -> "Please select a release to view")
+               Redirect(routes.EgaReleases.viewEgaReleases()).withSession(newSession)
+            }
+         })
+   }
+
+   def releaseNameAlreadyExists(releaseName: String): Boolean = {
+      if (getReleaseNames() contains releaseName) {
+         return true
+      } else {
+         return false
+      }
+   }
+
+   def getReleaseNames(): List[String] = {
+      return List("Release 15 2013-11-21", "Release 15a 2013-10-01", "Release 13 2013-09-22")
+   }
+}
