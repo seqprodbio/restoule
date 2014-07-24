3c7e605ab75f2c3c11a05e2d91ef0369391d3435
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-07-24 13:58:59 -0400

    Added additional fields in the sample_file and extracted information from the filename to populate those fields

--- app/models/SampleFile.scala
+++ app/models/SampleFile.scala
@@ -2,14 +2,21 @@ package models;
 
 import play.api.db.slick.Config.driver.simple._
 
-case class SampleFile(id: Option[Int], fileName: String, path: String, origin: String, createdInDB: java.sql.Timestamp)
+case class SampleFile(id: Option[Int], fileName: String, path: String, origin: String, swid: Int, sample: String, library: String, sequencerRunName: String, lane: Int, barcode: String, read: Int, createdInDB: java.sql.Timestamp)
 
 class SampleFileTable(tag: Tag) extends Table[SampleFile](tag, "sample_file") {
    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
    def fileName = column[String]("filename", O.NotNull)
    def path = column[String]("path", O.NotNull)
    def origin = column[String]("origin", O.NotNull)
+   def swid = column[Int]("swid", O.NotNull)
+   def sample = column[String]("sample", O.NotNull)
+   def library = column[String]("library", O.NotNull)
+   def sequencerRunName = column[String]("sequencer_run", O.NotNull)
+   def lane = column[Int]("lane", O.NotNull)
+   def barcode = column[String]("barcode", O.NotNull)
+   def read = column[Int]("read", O.NotNull)
    def createdInDB = column[java.sql.Timestamp]("created_tstmp", O.NotNull)
 
-   def * = (id.?, fileName, path, origin, createdInDB) <> (SampleFile.tupled, SampleFile.unapply)
+   def * = (id.?, fileName, path, origin, swid, sample, library, sequencerRunName, lane, barcode, read, createdInDB) <> (SampleFile.tupled, SampleFile.unapply)
 }
--- app/models/persistance/SampleFileDAO.scala
+++ app/models/persistance/SampleFileDAO.scala
@@ -5,6 +5,9 @@ import models.SampleFileTable
 
 import play.api.db.slick.Config.driver.simple._
 
+import scala.util.matching.Regex
+import scala.collection.immutable.StringOps
+
 object SampleFileDAO {
 
    val sampleFiles = TableQuery[SampleFileTable]
@@ -40,7 +43,63 @@ object SampleFileDAO {
    def createSampleFile(path: String, origin: String) = { implicit session: Session =>
       var pathParts = path.split("/")
       var fileName = pathParts(pathParts.length - 1)
-      var newSampleFile = new SampleFile(None, fileName, path, origin, new java.sql.Timestamp(System.currentTimeMillis()))
+      var fileNameParts = fileName.split("\\.")
+      var fileNameWithoutExtension = fileNameParts(0)
+      var swid = 0
+      var sample = ""
+      var library = ""
+      var sequencerRunName = ""
+      var lane = 0
+      var barcode = ""
+      var read = 0
+      var regex = "^(SWID_[0-9]{4,6}_)?([A-Z]{3,5})_([0-9]{3,4}|[0-9][CR][0-9]{1,2})_(nn|[A-Z]{1}[a-z]{1})_([nRPXMCFE])_(SE|PE|MP)_(nn|[0-9]{2,4}|[0-9]K)_(TS|EX|CH|BS|WG|TR|WT|SM|MR)_(NoIndex|[0-9]*_[a-zA-Z0-9]*_[0-9]*_[a-zA-Z0-9-]*_?([A-Z]{2,})?)(_NoIndex|_sd-seq|_mm)?_([0-9]|L[0-9]{3})(_NoIndex)?_(R[0-9])(_[0-9]{3})?$".r
+      if (regex.findFirstMatchIn(fileNameWithoutExtension).isDefined) {
+         var dataMatch = regex.findFirstMatchIn(fileNameWithoutExtension).get
+         if (dataMatch.group(1) != null) {
+            println(dataMatch.group(1).substring(5, dataMatch.group(1).length - 1))
+            try {
+               swid = dataMatch.group(1).substring(5, dataMatch.group(1).length - 1).toInt
+            } catch {
+               case e: Exception => swid = 0
+            }
+         }
+         if (dataMatch.group(2) != null && dataMatch.group(3) != null && dataMatch.group(4) != null && dataMatch.group(5) != null) {
+            sample = dataMatch.group(2) + "_" + dataMatch.group(3) + "_" + dataMatch.group(4) + "_" + dataMatch.group(5)
+            if (dataMatch.group(6) != null && dataMatch.group(7) != null && dataMatch.group(8) != null) {
+               library = sample + "_" + dataMatch.group(6) + "_" + dataMatch.group(7) + "_" + dataMatch.group(8)
+            }
+         }
+         if (dataMatch.group(9) != null) {
+            sequencerRunName = dataMatch.group(9)
+         }
+         if (dataMatch.group(12) != null) {
+            if (dataMatch.group(12).charAt(0).equals('L')) {
+               try {
+                  lane = dataMatch.group(12).substring(1).toInt
+               } catch {
+                  case e: Exception => lane = 0
+               }
+            } else {
+               try {
+                  lane = dataMatch.group(12).toInt
+               } catch {
+                  case e: Exception => lane = 0
+               }
+            }
+         }
+         if (dataMatch.group(13) != null && !dataMatch.group(13).equals("_NoIndex")) {
+            barcode = dataMatch.group(13)
+         }
+         if (dataMatch.group(14) != null) {
+            try {
+               read = dataMatch.group(14).substring(1).toInt
+            } catch {
+               case e: Exception => read = 0
+            }
+         }
+
+      }
+      var newSampleFile = new SampleFile(None, fileName, path, origin, swid, sample, library, sequencerRunName, lane, barcode, read, new java.sql.Timestamp(System.currentTimeMillis()))
       sampleFiles.insert(newSampleFile)
    }
 
